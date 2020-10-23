40bc18c6a852e6a9181e01e3f4ad9235cf5ec375
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-10-23 12:46:06 +0900

    add byte_array_controllers (not tested)

---
+++ byte_array_controllers/include/byte_array_controllers/common_namespaces.hpp
@@ -0,0 +1,13 @@
+#ifndef BYTE_ARRAY_CONTROLLERS_COMMON_NAMESPACES_HPP
+#define BYTE_ARRAY_CONTROLLERS_COMMON_NAMESPACES_HPP
+
+namespace hardware_interface {}
+
+namespace hardware_interface_extensions {}
+
+namespace byte_array_controllers {
+namespace hi = hardware_interface;
+namespace hie = hardware_interface_extensions;
+} // namespace byte_array_controllers
+
+#endif
