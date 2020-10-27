916f309c5a1b3d7b702930bb141a57b96407f20f
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-10-27 17:21:14 +0900

    remove byte_array_controller

--- byte_array_controllers/src/byte_array_controllers.cpp
+++
@@ -1,9 +0,0 @@
-#include <byte_array_controllers/byte_array_command_controller.hpp>
-#include <byte_array_controllers/byte_array_state_controller.hpp>
-#include <controller_interface/controller_base.h>
-#include <pluginlib/class_list_macros.h>
-
-PLUGINLIB_EXPORT_CLASS(byte_array_controllers::ByteArrayStateController,
-                       controller_interface::ControllerBase);
-PLUGINLIB_EXPORT_CLASS(byte_array_controllers::ByteArrayCommandController,
-                       controller_interface::ControllerBase);
